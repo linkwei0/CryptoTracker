b67625279d8cc5f48021007d56333a49b84c6902
Artem Batsanov <mr.bartem@mail.ru>
2025-03-11 19:10:03 +0300

    Initial Commit

---
+++ CryptoTracker/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  CryptoTracker
+//
+//  Created by Артём Бацанов on 11.03.2025.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
